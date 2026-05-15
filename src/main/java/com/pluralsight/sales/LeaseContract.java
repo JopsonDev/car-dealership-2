3a96193b185833c99998ff3e6e36cdc2f01b6bcf
JopsonDev <jopsonmason@outlook.com>
2026-05-15 11:31:46 -0400

    added leaseContract classes and added inventory

---
+++ src/main/java/com/pluralsight/sales/LeaseContract.java
@@ -0,0 +1,50 @@
+package com.pluralsight.sales;
+
+import com.pluralsight.Vehicle;
+
+public class LeaseContract extends Contract {
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String date, String name, String email, Vehicle vehicleSold) {
+        super(date, name, email, vehicleSold);
+        this.expectedEndingValue = getVehicleSold().getPrice() / 2;
+        this.leaseFee = getVehicleSold().getPrice() * .07;
+    }
+
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public void setExpectedEndingValue(double expectedEndingValue) {
+        this.expectedEndingValue = expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    public void setLeaseFee(double leaseFee) {
+        this.leaseFee = leaseFee;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return (getVehicleSold().getPrice() - expectedEndingValue) + leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 36;
+        double interestRate = 4.0 / 1200;
+        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+        monthlyPayment = Math.round(monthlyPayment * 100);
+        monthlyPayment /= 100;
+        return monthlyPayment;
+    }
+
+    @Override
+    public String toString() {
+        return expectedEndingValue + "|" + leaseFee + "|" + getTotalPrice() + "|" + getMonthlyPayment();
+    }
+}
